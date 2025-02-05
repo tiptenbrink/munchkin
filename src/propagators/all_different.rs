d3d6f4842bc6821219f988b1a28ef41c490851d7
ImkoMarijnissen <imko.marijnissen@gmail.com>
2025-02-05 11:35:07 +0100

    feat: adding test cases for all-different and maximum

--- src/propagators/all_different.rs
+++ src/propagators/all_different.rs
@@ -38,14 +38,28 @@ impl<Var: IntegerVariable + 'static> Propagator for AllDifferentPropagator<Var> 
 mod tests {
     use crate::engine::test_helper::TestSolver;
 
+    use super::AllDifferentPropagator;
+
     #[test]
     fn test_simple_propagation() {
         let mut solver = TestSolver::default();
 
-        let x = solver.new_variable(5, 5);
-        let y = solver.new_variable(6, 6);
-        let y = solver.new_variable(5, 7);
+        let x1 = solver.new_variable(0, 3);
+        let x2 = solver.new_variable(0, 3);
+        let x3 = solver.new_variable(0, 3);
+        let x4 = solver.new_variable(1, 2);
+        let x5 = solver.new_variable(-2, 6);
+        let x6 = solver.new_variable(1, 6);
 
-        // let _ = solver.new_propagator();
+        let variables = [x1, x2, x3, x4, x5, x6];
+
+        let result = solver
+            .new_propagator(AllDifferentPropagator::new(variables.into()))
+            .expect("Expected no error");
+
+        solver.assert_bounds(x6, 4, 6);
+        for value in 0..=3 {
+            assert!(solver.contains(x5, value));
+        }
     }
 }
