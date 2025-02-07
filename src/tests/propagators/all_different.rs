bfb46aab0f2d87fad6397c3adce11cb2523916da
Maarten Flippo <maartenflippo@outlook.com>
2025-02-07 14:57:59 +0100

    Added extra test case for all different

--- src/tests/propagators/all_different.rs
+++ src/tests/propagators/all_different.rs
@@ -4,7 +4,7 @@ use crate::engine::test_helper::TestSolver;
 use crate::propagators::all_different::AllDifferentPropagator;
 
 #[test]
-fn test_simple_propagation() {
+fn test_bounds_propagation() {
     let mut solver = TestSolver::default();
 
     let x1 = solver.new_variable(0, 3);
@@ -24,4 +24,23 @@ fn test_simple_propagation() {
     for value in 0..=3 {
         assert!(solver.contains(x5, value));
     }
+}
+
+#[test]
+fn test_holes_are_punched() {
+    // Taken from https://www.lnmb.nl/conferences/2014/programlnmbconference/Shaw-1.pdf
+    let mut solver = TestSolver::default();
+
+    let x1 = solver.new_sparse_variable(&[1, 2]);
+    let x2 = solver.new_sparse_variable(&[3, 4]);
+    let x3 = solver.new_sparse_variable(&[1, 3]);
+    let x4 = solver.new_sparse_variable(&[3, 4]);
+    let x5 = solver.new_sparse_variable(&[2, 4, 5, 6]);
+    let x6 = solver.new_sparse_variable(&[5, 6, 7]);
+
+    let _ = solver
+        .new_propagator(AllDifferentPropagator::new([x1, x2, x3, x4, x5, x6].into()))
+        .expect("Expected no error");
+
+    solver.assert_bounds(x4, 4, 4);
 }
