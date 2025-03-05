bceb9720bb30aaf31856afda9d45921eba31c76c
ImkoMarijnissen <imko.marijnissen@gmail.com>
2025-03-05 13:28:19 +0100

    feat: adding test case for lbd calculation

--- src/engine/sat/lbd_calculator.rs
+++ src/engine/sat/lbd_calculator.rs
@@ -6,3 +6,33 @@ use crate::variables::Literal;
 pub(crate) fn calculate_lbd(_clause: &[Literal], _assignments: &AssignmentsPropositional) -> usize {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::engine::sat::calculate_lbd;
+    use crate::engine::test_helper::TestSolver;
+
+    #[test]
+    fn simple_compute_lbd() {
+        let mut solver = TestSolver::default();
+
+        let x = solver.new_literal();
+        let y = solver.new_literal();
+        let z = solver.new_literal();
+        let a = solver.new_literal();
+
+        solver.assignments_propositional.increase_decision_level();
+        solver.assignments_propositional.enqueue_decision_literal(x);
+
+        solver.assignments_propositional.increase_decision_level();
+        solver.assignments_propositional.enqueue_decision_literal(y);
+        solver.assignments_propositional.enqueue_decision_literal(z);
+
+        solver.assignments_propositional.increase_decision_level();
+        solver.assignments_propositional.enqueue_decision_literal(a);
+
+        let lbd = calculate_lbd(&[a, x, y, z], &solver.assignments_propositional);
+
+        assert_eq!(lbd, 2);
+    }
+}
